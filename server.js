0d4707de55f6526f486e1c2af60c5a01b88447ca
Tyler Malone <tylerm0424@yahoo.com>
2023-11-02 21:32:08 -0700

    set up handlebars files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/helper');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -12,12 +12,25 @@ const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+// app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.get('/homepage', (req, res) => {
+  res.render('homepage');
+});
+
+app.get('/exercises', (req, res) => {
+  res.render('exercises');
+});
+
+app.get('/caloricNeeds', (req, res) => {
+  res.render('caloricNeeds');
+});
+
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
