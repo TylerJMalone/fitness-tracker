f05b43c9092a199a15cdbd100ad6c1a897c52165
Tyler Malone <tylerm0424@yahoo.com>
2023-11-03 11:59:29 -0700

    added code to seeds and models folders

---
+++ seeds/seed.js
@@ -0,0 +1,17 @@
+const sequelize = require('./config/connection');
+const User = require('./models/User');
+
+const UserData = require ('./userData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(UserData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
