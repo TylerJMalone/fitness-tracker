671d922d517ace4bb835b76f738aaaf7bd216322
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-11-06 17:22:07 -0800

    favorite model

---
+++ models/Favorite.js
@@ -0,0 +1,43 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Favorite extends Model {}
+
+Favorite.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        type: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        muscle: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        difficulty: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        instructions: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    },
+    {
+            sequelize,
+            freezeTableName: true,
+            modelName: 'favorite'
+        }
+    );
+
+module.exports = Favorite;
