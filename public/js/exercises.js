04b15bd8e9249c76323c1b6a377a280132ef62b6
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-11-09 15:57:06 -0800

    edit variable name

--- public/js/exercises.js
+++ public/js/exercises.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     $.ajax({
       url: 'https://api.api-ninjas.com/v1/exercises',
       headers: {
-        'X-Api-Key': `'${API_KEY}'`
+        'X-Api-Key': `'${process.env.API_KEY}'`
       },
       data: {
         muscle: muscle,
